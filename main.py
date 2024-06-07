c9c30d333a9bebdc524a0edb6cf4e6b5f825b2e9
tarerere <165239593+tarerere@users.noreply.github.com>
2024-06-08 02:46:04 +0900

    Update main.py
    強制退出完成版

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # 2024/06/07 バージョン
 import discord
 import os
-from datetime import datetime, timedelta
+import datetime
 from keep import keep_alive
 import asyncio
 import time
@@ -13,16 +13,16 @@ client = discord.Client(intents=discord.Intents.default())
 SERVER_ID = "1214572848211955733"
 # 通知させるチャンネルのID
 ALERT_CHANNEL = 1214573763526656050
-SHERE= 1214620402035200060
+SHERE_ID= 1214620402035200060
 #テスト用　ALERT_CHANNEL = 1214572849059201045
 # 通話参加時のみ付与させるロールのID
-ROLE_ID = 1222180449892434120
+ROLE_ID = '1222180449892434120'
 # 通知をメンションするロールID(Rhythmとか)
 ROLE_ID2 = "1214576516365549578"
 # 通知を除外させたいメンバーID(Rhythmとか)
 EXCLUDE_ID = 000000000
 # 通知を除外させたいチャンネルID（0：運動用　1：運動後チル）
-LIST_NOALERT_CHANNEL = ['1222780507771633715','1248475229593014403']
+LIST_NOALERT_CHANNEL = [1222780507771633715,1248475229593014403]
 # TTS
 TTS = True
 #now = datetime.utcnow() + timedelta(hours=9)
@@ -31,17 +31,26 @@ TTS = True
 #起動時処理
 async def on_ready():
 	last_clocked_time = datetime.datetime.now()	
-	await SHERE.send('test')
+	shere_channel = client.get_channel(SHERE_ID)
+	rinfit_channel = client.get_channel(LIST_NOALERT_CHANNEL[0])
+
 	while True:
 		#運動部チャンネルに1人でもいたら通知
-		if len(LIST_NOALERT_CHANNEL[0].channel.members) >= 1:
+		if len(rinfit_channel.voice_states.keys()) >= 1:
 			#ここから30秒間隔の処理
 			if last_clocked_time.strftime('%H:%M') is not datetime.datetime.now().strftime('%H:%M'):
 				# 1時半に強制退出
-				if datetime.datetime.now().strftime('%H%M') >= '140':
-					await SHERE.send('<@&' + ROLE_ID2 + '>' + '30秒後に強制退出がまもなく実行されます。本日も運動お疲れ様でした！', tts=TTS)
+				if datetime.datetime.now().strftime('%H%M') >= '130':
+					await SHERE_ID.send('30秒後に強制退出がまもなく実行されます。本日も運動お疲れ様でした！', tts=TTS)
 					time.sleep(30)
-					await move_to_none()
+					talk_channel_id = LIST_NOALERT_CHANNEL[0] 
+					# チャンネル経由でサーバー内のボイスチャンネル全体を走査
+					for ch in shere_channel.guild.voice_channels:
+						for member in ch.members:
+							if ch.id == rinfit_channel.id:
+								# move_to(None)で特定のメンバーを切断する
+								await member.move_to(None)
+							
 		last_clocked_time = datetime.datetime.now() #時刻更新処理
 		await asyncio.sleep(30)
 
@@ -75,16 +84,3 @@ try:
   client.run(os.environ['TOKEN'])
 except:
   os.system("kill")
- 
-# 強制退出処理
-async def move_to_none():
-	# 接続に利用するオブジェクト
-	client = discord.Client()
-	# 該当サーバーの特定のチャンネルIDを入れてください。
-	talk_channel_id = LIST_NOALERT_CHANNEL[0] 
-	channel = client.get_channel(talk_channel_id)
-	# チャンネル経由でサーバー内のボイスチャンネル全体を走査
-	for ch in channel.guild.voice_channels:
-		for member in ch.members:
-			# move_to(None)で特定のメンバーを切断する
-			await member.move_to(None)
