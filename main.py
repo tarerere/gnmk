6f206fc252f562a99d04cf585fcf1253dceca485
tarerere <165239593+tarerere@users.noreply.github.com>
2024-06-07 23:00:25 +0900

    Update main.py
    修正

--- main.py
+++ main.py
@@ -25,21 +25,18 @@ LIST_NOALERT_CHANNEL = ['1222780507771633715','1248475229593014403']
 # TTS
 TTS = True
 #now = datetime.utcnow() + timedelta(hours=9)
-#非通知フラグ
-NOALERT_FLG = 0
 
 @client.event
 #起動時処理
 async def on_ready():
-	last_clocked_time = datetime.datetime.now()
-				
+	last_clocked_time = datetime.datetime.now()			
 	while True:
-        #運動部チャンネルに1人でもいたら通知
+		#運動部チャンネルに1人でもいたら通知
 		if len(LIST_NOALERT_CHANNEL[0].channel.members) >= 1:
 			#ここから30秒間隔の処理
-			if last_clocked_time('%H:%M') is not datetime.datetime.now('%H:%M'):
+			if last_clocked_time.strftime('%H:%M') is not datetime.datetime.now().strftime('%H:%M'):
 				# 1時半に強制退出
-				if datetime.datetime.now('%H:%M') == '1:30':
+				if datetime.datetime.now().strftime('%H%M') >= '130':
 					await LIST_NOALERT_CHANNEL[0].send('<@&' + ROLE_ID + '>' + '30秒後に強制退出がまもなく実行されます。本日も運動お疲れ様でした！', tts=TTS)
 					time.sleep(30)
 					await move_to_none()
@@ -48,40 +45,38 @@ async def on_ready():
 		await asyncio.sleep(30)
 
 async def on_voice_state_update(member, before, after):
-  # 通話チャンネルの状態を監視、入退室がトリガー
-  # 非通知用のチャンネルの場合は処理を終了する。
- for i in LIST_NOALERT_CHANNEL:
-  if str(after.channel.id) != i:
-    NOALERT_FLG = 1
-
-  if str(member.guild.id) == SERVER_ID and (str(before.channel) != str(after.channel)) and NOALERT_FLG != 0:
-    # メッセージを送るチャンネル
-    alert_channel = client.get_channel(ALERT_CHANNEL)
-    if member.id != EXCLUDE_ID:
-      # 通話参加時に付与するロールを取得
-      role = member.guild.get_role(ROLE_ID)
-      # 入室か退室かを判定
-      if before.channel is None:
-        await member.add_roles(role)
-        if len(after.channel.members) == 1:
-            if member.nick is None:
-              msg = '<@&' + ROLE_ID2 + '>' + f'{after.channel.name} に ' + f'{member.name} が参加しました。'
-              await alert_channel.send(msg)
-            else:
-              msg = '<@&' + ROLE_ID2 + '>' + f'{after.channel.name} に ' + f'{member.nick} が参加しました。'
-              await alert_channel.send(msg, tts=TTS)
-      elif after.channel is None:
-        await member.remove_roles(role)
-        #メッセージを削除する
-        # def delete_messages(,,,):
-
-keep_alive()
+	noalertflg = 0
+# 通話チャンネルの状態を監視、入退室がトリガー
+# 非通知用のチャンネルの場合は処理を終了する。
+	for i in LIST_NOALERT_CHANNEL:
+		if str(after.channel.id) != i:
+			noalertflg = 1
+	if str(member.guild.id) == SERVER_ID and (str(before.channel) != str(after.channel)) and noalertflg != 0:
+		# メッセージを送るチャンネル
+		alert_channel = client.get_channel(ALERT_CHANNEL)
+		if member.id != EXCLUDE_ID:
+			# 通話参加時に付与するロールを取得
+			role = member.guild.get_role(ROLE_ID)
+			# 入室か退室かを判定
+			if before.channel is None:
+				await member.add_roles(role)	
+		if len(after.channel.members) == 1:
+			if member.nick is None:
+				msg = '<@&' + ROLE_ID2 + '>' + f'{after.channel.name} に ' + f'{member.name} が参加しました。'
+				await alert_channel.send(msg)
+			else:
+				msg = '<@&' + ROLE_ID2 + '>' + f'{after.channel.name} に ' + f'{member.nick} が参加しました。'
+				await alert_channel.send(msg, tts=TTS)
+		elif after.channel is None:			
+			await member.remove_roles(role)
+			#メッセージを削除する
+			# def delete_messages(,,,):
+	keep_alive()
 try:
-  client.run(os.environ['TOKEN'])
+	client.run(os.environ['TOKEN'])
 except:
-  os.system("kill")
+	os.system("kill")
  
-  
 # 強制退出処理
 async def move_to_none():
 	# 接続に利用するオブジェクト
@@ -94,5 +89,3 @@ async def move_to_none():
 		for member in ch.members:
 			# move_to(None)で特定のメンバーを切断する
 			await member.move_to(None)
-
-
