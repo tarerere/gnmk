67df0355a4eea3e4c9cb4847de476059c87b51ac
tarerere <165239593+tarerere@users.noreply.github.com>
2024-08-02 23:21:25 +0900

    Update main.py

--- main.py
+++ main.py
@@ -60,11 +60,9 @@ async def on_ready():
 					for member in ch.members:
 						if ch.id == rinfit_channel.id:
 							# move_to(None)で特定のメンバーを切断する
-							await member.move_to(None)
-							
-							
-			last_clocked_time = datetime.datetime.now() #時刻更新処理
-			await asyncio.sleep(30)
+							await member.move_to(None)		
+		last_clocked_time = datetime.datetime.now() #時刻更新処理
+		await asyncio.sleep(30)
 
 def kyouseiKill(now):
 	msg = ''
